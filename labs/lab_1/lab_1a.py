37f5bcd7e1b0c1ff89724ee0c3641a4ef8b0804b
Dominic Camill <106774701+LikeMyGames@users.noreply.github.com>
2026-04-19 14:23:40 -0700

    added intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Dominic Camill" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Introduction: Hey everyone, I'm Dominic Camill, a junior from California. I am one of the software leads for my high school's FRC team, mainly focusing on the autonomous functionality of the robot. I also enjoy 3d printing and I play the baritone saxophone. I'm excited to learn new things through this course.")
 
 if __name__ == "__main__":
     main()
