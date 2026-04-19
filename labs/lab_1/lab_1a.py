5de63db4e8df50d570f22242eaddc7031207f301
Dominic Camill <106774701+LikeMyGames@users.noreply.github.com>
2026-04-19 14:59:28 -0700

    moved robot speed in lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,6 +3,8 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+
+This is to simulate a change made on a robot: robot_speed = 3 # m/s
 """
 
 def main():
@@ -12,8 +14,6 @@ def main():
 
     print(f"{name}, Welcome to the CSS course!")
     print("Introduction: Hey everyone, I'm Dominic Camill, a junior from California. I am one of the software leads for my high school's FRC team, mainly focusing on the autonomous functionality of the robot. I also enjoy 3d printing and I play the baritone saxophone. I'm excited to learn new things through this course.")
-
-    robot_speed = 5 # m/s
 
 if __name__ == "__main__":
     main()
